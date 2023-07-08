4b0a9677ef2a8ecd66d51a72f5c90c3c02ceac68
Yonatan Shaked <theproguy12@gmail.com>
2023-07-08 17:01:37 +0300

    Update Constants.java

--- app/src/main/java/com/example/homework1/interfaces/Constants.java
+++ app/src/main/java/com/example/homework1/interfaces/Constants.java
@@ -4,7 +4,5 @@ public interface Constants {
     int ANIM_DUR = 10000;
     int LOCATION_REQUEST_CODE = 10001;
     String SP_FILE = "MY_SP";
-    String KEY_TOP_TEN = "KEY_TOP_TEN";
     int CATCH_CHANCE = 5;
-
 }
